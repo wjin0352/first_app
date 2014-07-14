c4a8c722210d7ae89d91fda9c551047d88da1491
won <wjin@nyit.edu>
2014-07-14 04:06:12 -0400

    microposts modified

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,12 @@
 class Micropost < ActiveRecord::Base
 	validates :content, length: {maximum: 140 }
 end
+
+class User < ActiveRecord::Base
+	has_many :Microposts
+end
+
+class Micropost < ActiveRecord::Base
+	belongs_to :user
+	validates :content, length: {maximum: 140}
+end
